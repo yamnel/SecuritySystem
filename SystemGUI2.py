09009fafef6d21d626b77dfd7e0db91fdefeced0
Yamnel <Yamnel@FapTop.local>
2016-12-02 07:44:51 -0500

    Fixed names and added some comments.

--- SystemGUI2.py
+++ SystemGUI2.py
@@ -7,10 +7,8 @@ class SystemGUI(QWidget):
     def __init__(self):
         super().__init__()
         # holders hold the text entered by the user and create a string.
-        self.pin_holder = []
-        self.card_holder = []
-
-
+        self.pin_buffer = []
+        self.card_buffer = []
 
         self.card_label = QLabel("Card: ")
         self.card_input = QLineEdit()
@@ -19,19 +17,12 @@ class SystemGUI(QWidget):
         self.pin_input = QLineEdit()
 
         self.initUI()
-        # self.main()
 
-
-        # ------------ Main() -------------------------------
-        # creating a test user
-        self.usr = User(user_id='8108', pin='0000', card='123')
-
-        # what the user uses to enter the room
-        self.usr_credentials = [('pin', 'card')]
-
-        self.room_number = '429'
-
-        # pin_pad_input = (str(self.userNameEdit), str(self.showPassEdit))
+        # ------------ from Database -------------------------------
+        self.usr = User(user_id='8108', pin='0000', card='123')  # creating a test user
+        self.usr_credentials = [('pin', 'card')]  # what the user uses to enter the room
+        self.room_number = '429'  # room we are entering
+        # ------------ end Database -------------------------------
 
     # ActionListener
     def buttonClick(self):
@@ -39,64 +30,33 @@ class SystemGUI(QWidget):
 
         # Handle Clear button
         if (digit.text() == 'Clear'):
-            # print("clr pressed, clearing")  # TESTING
-            self.pin_holder = []
+            self.pin_buffer = []
             self.card_input.setText("")
 
-            self.card_holder = []
+            self.card_buffer = []
             self.pin_input.setText("")
             return
 
         # Handle Enter button
         if (digit.text() == "Enter"):
-            # print("Enter was pressed")  # TESTING
-
-            self.card_holder = self.pin_input.text()
-            # print(self._userName)
+            self.card_buffer = self.pin_input.text()
 
             pin_pad_input = (str(self.pin_input.text()), str(self.card_input.text()))
             print(pin_pad_input)
 
             self.check_access(self.usr, self.room_number, self.usr_credentials, pin_pad_input)
-
-            # self.close()
             return
 
-        # Test-----
-        # print(digit.text() + " was pressed")
-
-        # #Test -----
-        # print(int(''.join((map(str, self.__password)))))
-        #
-
-
-        if ((len(self.pin_holder) < 4)):
+        if ((len(self.pin_buffer) < 4)):
             tempDigit = int(digit.text())
-            self.pin_holder.append(tempDigit)
-
-            # Test -----
-            # print(int(''.join((map(str, self.pin_holder)))))
-            # str1 = ''.join(str(e) for e in self.pin_holder)
-
-            self.pin_input.setText(''.join(str(e) for e in self.pin_holder))
+            self.pin_buffer.append(tempDigit)
+            self.pin_input.setText(''.join(str(e) for e in self.pin_buffer))
 
         else:
             tempDigit = int(digit.text())
-            self.card_holder.append(tempDigit)
+            self.card_buffer.append(tempDigit)
+            self.card_input.setText(''.join(str(e) for e in self.card_buffer))
 
-            # Test -----
-            # print(int(''.join((map(str, self.card_holder)))))
-            # str1 = ''.join(str(e) for e in self.card_holder)
-
-            self.card_input.setText(''.join(str(e) for e in self.card_holder))
-
-            # ----------------------End buttonClick------------------------
-
-    # Getters
-    # def get_password(self):
-    #     return self.__password
-    # def get_userName(self):
-    #     return self._userName
 
     def initUI(self):
         grid = QGridLayout()
@@ -129,15 +89,14 @@ class SystemGUI(QWidget):
         self.show()
         # ----------------------End initUI--------------------------------
 
-    # the password is the input from the user
-    # the input_type is the source of the input
-    # ( ex. card reader, pin pad, biometrics scanner)
-
+    """
+    the password is the input from the user
+    the input_type is the source of the input
+    ( ex. card reader, pin pad, biometrics scanner)
+    """
     def check_access(self, user, room, input_type, password):
-        inp = self.switch(user, input_type)
 
-        # print("\nThe user's saved passwords are: ", ", ".join(inp), "\nThe entered passwords were: ",
-        #       ", ".join(password), "\n")  # Testing
+        inp = self.switch(user, input_type)
 
         if password == inp:  # this checks both passwords are correct
             print("Password verified!")
@@ -147,7 +106,7 @@ class SystemGUI(QWidget):
                     print("Door Opens!")
                 else:
                     print("You don't have access to this room!")
-            # catches
+
             except KeyError:
                 print("You don't have access to this room!")
         else:
@@ -157,15 +116,16 @@ class SystemGUI(QWidget):
                 print("Admin was not reached!!! ", Exception)
             print("Invalid Password!")
 
-        self.pin_holder, self.card_holder = [], []
+        self.pin_buffer, self.card_buffer = [], []
         self.card_input.setText("")
         self.pin_input.setText("")
 
+    """
+    It works like a switch does in other languages.
+    It returns the value of the access type
+    depending on the input source
+    """
 
-
-    # it returns the value of the access type
-    # depending on the input source
-    # it works like a switch does in other languages
     def switch(self, user, inp):
         l = {
             'pin': user.get_pin(),
@@ -178,9 +138,9 @@ class SystemGUI(QWidget):
             return l[val1], l[val2]
 
     def alert_admin(self):
-        #user_ip = 'Yamnel@10.0.0.6'
+        # user_ip = 'Yamnel@10.0.0.6'
         user_ip = 'Yamnel@10.100.201.34'
-        
+
         path = 'python3 ~/Desktop/Warning.py'
         try:
             os.system('ssh {} nohup python /Users/Yamnel/Desktop/Warning.py &'.format(user_ip))
