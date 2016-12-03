84fd1af278e1ec12a7e4024e92444e7d5af0ad71
Yamnel <Yamnel@FapTop.local>
2016-12-02 20:08:05 -0500

    deleted these files b/c they only run on Pi.

--- enrolar.py
+++
@@ -1,9 +0,0 @@
-import FPS, sys
-from test_raw import *
-
-if __name__ == '__main__':
-    fps = FPS.FPS_GT511C3()
-    fps.UseSerialDebug = True
-    Enroll(fps,sys.argv[1])
-    fps.Close()
-    pass
--- verificar.py
+++
@@ -1,9 +0,0 @@
-import FPS, sys
-from test_raw import *
-
-if __name__ == '__main__':
-    fps = FPS.FPS_GT511C3()
-    fps.UseSerialDebug = True
-    print (Verify(fps,sys.argv[1]))
-    fps.Close()
-    pass
